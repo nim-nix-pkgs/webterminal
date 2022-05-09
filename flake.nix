eb1ce1decfb2eb94a1b4e6361cfc57e90fc15d79
hugosenari <hugosenari@gmail.com>
2022-05-09 20:45:20 -0300

    chore: re index webterminal

--- flake.nix
+++ flake.nix
@@ -7,30 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."webterminal-master".dir   = "master";
-  inputs."webterminal-master".owner = "nim-nix-pkgs";
-  inputs."webterminal-master".ref   = "master";
-  inputs."webterminal-master".repo  = "webterminal";
-  inputs."webterminal-master".type  = "github";
-  inputs."webterminal-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."webterminal-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."webterminal-v0_0_1".dir   = "v0_0_1";
-  inputs."webterminal-v0_0_1".owner = "nim-nix-pkgs";
-  inputs."webterminal-v0_0_1".ref   = "master";
-  inputs."webterminal-v0_0_1".repo  = "webterminal";
-  inputs."webterminal-v0_0_1".type  = "github";
-  inputs."webterminal-v0_0_1".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."webterminal-v0_0_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."webterminal-v0_0_2".dir   = "v0_0_2";
-  inputs."webterminal-v0_0_2".owner = "nim-nix-pkgs";
-  inputs."webterminal-v0_0_2".ref   = "master";
-  inputs."webterminal-v0_0_2".repo  = "webterminal";
-  inputs."webterminal-v0_0_2".type  = "github";
-  inputs."webterminal-v0_0_2".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."webterminal-v0_0_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
